c00b92f53531ee2177817d78cb434b67ad3b563f
Julian Guarin <devsavant.jguarin@intelepeer.com>
2025-02-05 09:36:06 -0500

    barbar configurations to display the buffer number

--- lua/config/plugins/barbar.lua
+++ lua/config/plugins/barbar.lua
@@ -1,12 +1,24 @@
 return {
-    {
-        'romgrk/barbar.nvim',
-        dependencies = {
-            'lewis6991/gitsigns.nvim',
-            'nvim-tree/nvim-web-devicons',
-        },
-        init = function() vim.g.barbar_auto_setyp = false end,
-        opts = {},
-        version = '^1.9.1',
-    },
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+			require("barbar").setup({
+				animation = true,
+				icons = {
+					gitsigns = {
+						added = { enabled = true, icon = "+" },
+						changed = { enabled = true, icon = "~" },
+					},
+					buffer_number = true,
+				},
+			})
+		end,
+		opts = {},
+		version = "^1.9.1",
+	},
 }
